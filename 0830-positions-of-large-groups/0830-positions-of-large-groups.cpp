d79214afeca2eb95d7128c8cb676ac9ecf545bfe
aitievan <124905950+aitievan@users.noreply.github.com>
2023-12-23 23:42:37 +0600

    Time: 2 ms (61.94%), Space: 7.8 MB (22.89%) - LeetHub

--- 0830-positions-of-large-groups/0830-positions-of-large-groups.cpp
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.cpp
@@ -5,13 +5,10 @@ public:
         vector<vector<int>> ans;
         
         for(int end = 0; end < S.size(); end++){
-            //check for a group when the string ends
             if(end == S.size()-1 || S[end] != S[end+1]){
-                //deal with old group
                 if(end-start+1 >= 3){
                     ans.push_back(vector<int> {start, end});
                 }
-                //update for new group
                 start = end+1;
             }
         }
